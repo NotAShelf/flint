078e7e4fabc626dbde47c3e59ba012a094a41fbd
NotAShelf <raf@notashelf.dev>
2024-12-22 11:33:30 +0300

    doc comments

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type Options struct {
 	FailIfMultipleVersions bool
 }
 
+// flakeURL constructs a dependency URL from a node definition
 func flakeURL(dep map[string]interface{}) string {
 	locked, ok := dep["locked"].(map[string]interface{})
 	if !ok {
@@ -43,9 +44,11 @@ func flakeURL(dep map[string]interface{}) string {
 	return ""
 }
 
+// analyzeFlake processes the flake.lock data to extract dependencies and their reverse dependencies
 func analyzeFlake(flakeLock map[string]interface{}) Flake {
 	deps := make(map[string][]string)
 	reverseDeps := make(map[string][]string)
+
 	nodes, _ := flakeLock["nodes"].(map[string]interface{})
 	for name, depInterface := range nodes {
 		dep, _ := depInterface.(map[string]interface{})
@@ -71,14 +74,17 @@ func analyzeFlake(flakeLock map[string]interface{}) Flake {
 	return Flake{Deps: deps, ReverseDeps: reverseDeps}
 }
 
+// parseArgs parses command-line arguments into an Options struct
 func parseArgs() Options {
 	var lockPath string
 	var verbose bool
 	var failIfMultipleVersions bool
+
 	flag.StringVar(&lockPath, "flake_lock", "flake.lock", "Path to flake.lock")
 	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
 	flag.BoolVar(&failIfMultipleVersions, "fail-if-multiple-versions", false, "Exit with error if multiple versions found")
 	flag.Parse()
+
 	return Options{
 		LockPath:               lockPath,
 		Verbose:                verbose,
@@ -86,43 +92,37 @@ func parseArgs() Options {
 	}
 }
 
+// printDependencies outputs dependencies with multiple versions and their reverse dependencies
 func printDependencies(deps map[string][]string, reverseDeps map[string][]string, verbose bool) {
 	hasMultipleVersions := false
 
-	// group dependencies by URL
 	for url, aliases := range deps {
-		// only print URLs that have multiple versions
 		if len(aliases) == 1 {
-			continue
+			continue // skip single-version dependencies
 		}
 
-		// print the repository header
+		// Print repository information
 		fmt.Printf("%s\n", url)
 		fmt.Println(strings.Repeat("=", len(url)))
 
-		// multiple versions detected, list them
+		// List aliases and their reverse dependencies
 		hasMultipleVersions = true
+
 		for _, alias := range aliases {
-			// print each alias with its dependencies
-			dependencies := reverseDeps[alias]
 			fmt.Printf("%s:\n", alias)
-			for _, dep := range dependencies {
+			for _, dep := range reverseDeps[alias] {
 				fmt.Printf("    - %s\n", dep)
 			}
 
-			// print verbose information if enabled
 			if verbose {
-				fmt.Printf("Verbose info: %s has %d dependencies\n", alias, len(dependencies))
+				fmt.Printf("Verbose info: %s has %d dependencies\n", alias, len(reverseDeps[alias]))
 			}
 		}
 	}
 
 	if hasMultipleVersions {
 		fmt.Println("Multiple versions found across repositories.")
-	}
-
-	// additional verbose information if no multiple versions are found
-	if verbose && !hasMultipleVersions {
+	} else if verbose {
 		fmt.Println("No multiple versions detected.")
 	}
 }
@@ -145,10 +145,10 @@ func main() {
 
 	flake := analyzeFlake(flakeLock)
 
-	// Print the dependencies in a structured and improved format
+	// Print the dependencies
 	printDependencies(flake.Deps, flake.ReverseDeps, options.Verbose)
 
-	// Exit with an error if there are multiple versions and the flag is set
+	// Exit with an error if multiple versions were found and the flag is set
 	if options.FailIfMultipleVersions && len(flake.Deps) > 0 {
 		os.Exit(1)
 	}
