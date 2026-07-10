56e8a7d257b671db1d83cc15e21e34e1e92c2b8a
NotAShelf <raf@notashelf.dev>
2026-07-11 02:54:12 +0300

    nix: cleanup source filter
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I317049d83822d6f9f2dad4f89be4be486a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -2,16 +2,17 @@
   lib,
   buildGoModule,
   ...
-}: let
-  fs = lib.fileset;
+}:
+buildGoModule (finalAttrs: {
+  pname = "flint";
   version = "0.3.0";
-in
-  buildGoModule {
-    pname = "flint";
-    inherit version;
 
-    src = lib.cleanSource (fs.toSource {
-      root = ../.;
+  src = let
+    fs = lib.fileset;
+    s = ../.;
+  in
+    fs.toSource {
+      root = s;
       fileset = fs.unions [
         ../cmd
         ../internal
@@ -20,17 +21,16 @@ in
         ../go.mod
         ../go.sum
       ];
-    });
-
-    vendorHash = null;
-
-    ldflags = ["-s" "-w" "-X main.version=${version}"];
-
-    meta = {
-      description = "Stupid simple utility for linting your flake inputs";
-      homepage = "https://github.com/notashelf/flint";
-      license = lib.licenses.mpl20;
-      mainProgram = "flint";
-      maintainers = [lib.maintainers.NotAShelf];
     };
-  }
+
+  vendorHash = null;
+  ldflags = ["-s" "-w" "-X main.version=${finalAttrs.version}"];
+
+  meta = {
+    description = "Stupid simple utility for linting your flake inputs";
+    homepage = "https://github.com/notashelf/flint";
+    license = lib.licenses.mpl20;
+    mainProgram = "flint";
+    maintainers = [lib.maintainers.NotAShelf];
+  };
+})
