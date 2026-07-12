acc584bba2c802218ca649ab1334a143329b85c2
NotAShelf <raf@notashelf.dev>
2026-07-12 11:15:57 +0300

    nix: infer package version from the Cargo manifest
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Id6231101406ccb48a15caeb6a3a0a3de6a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -6,37 +6,39 @@
   openssl,
   zlib,
   ...
-}:
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "flint";
-  version = "0.3.0";
+}: let
+  cargoTOML = lib.importTOML ../Cargo.toml;
+in
+  rustPlatform.buildRustPackage {
+    pname = "flint";
+    version = cargoTOML.version;
 
-  src = let
-    fs = lib.fileset;
-    s = ../.;
-  in
-    fs.toSource {
-      root = s;
-      fileset = fs.unions [
-        ../Cargo.toml
-        ../Cargo.lock
-        ../src
-      ];
+    src = let
+      fs = lib.fileset;
+      s = ../.;
+    in
+      fs.toSource {
+        root = s;
+        fileset = fs.unions [
+          ../Cargo.toml
+          ../Cargo.lock
+          ../src
+        ];
+      };
+
+    cargoLock.lockFile = ../Cargo.lock;
+
+    nativeBuildInputs = [pkg-config];
+    buildInputs = [libgit2 openssl zlib];
+
+    # Link against the system libgit2 rather than building the vendored copy.
+    env.LIBGIT2_NO_VENDOR = "1";
+
+    meta = {
+      description = "Stupid simple utility for linting your flake inputs";
+      homepage = "https://github.com/notashelf/flint";
+      license = lib.licenses.mpl20;
+      mainProgram = "flint";
+      maintainers = [lib.maintainers.NotAShelf];
     };
-
-  cargoLock.lockFile = ../Cargo.lock;
-
-  nativeBuildInputs = [pkg-config];
-  buildInputs = [libgit2 openssl zlib];
-
-  # Link against the system libgit2 rather than building the vendored copy.
-  env.LIBGIT2_NO_VENDOR = "1";
-
-  meta = {
-    description = "Stupid simple utility for linting your flake inputs";
-    homepage = "https://github.com/notashelf/flint";
-    license = lib.licenses.mpl20;
-    mainProgram = "flint";
-    maintainers = [lib.maintainers.NotAShelf];
-  };
-})
+  }
